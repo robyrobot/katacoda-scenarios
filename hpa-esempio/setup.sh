59a1c8f39af173e277f06ff8033138536cabb502
roby <riannone@trovimap.com>
2019-12-10 21:08:41 +0100

    update

--- hpa-esempio/setup.sh
+++ hpa-esempio/setup.sh
@@ -1,7 +1,9 @@
 stty -echo
-snap install microk8s --classic
-snap alias microk8s.kubectl kubectl
-microk8s.enable metrics-server dns
-echo "Done!"
-stty echo 
+echo -n "Kubernetes installing ..."
+{
+    snap install microk8s --classic
+    snap alias microk8s.kubectl kubectl
+    microk8s.enable metrics-server dns
+} &> /dev/null
+echo "Done!" && stty echo 
 
