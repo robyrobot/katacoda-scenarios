e32543f164c5e962798642406a3211f0870f4898
roby <riannone@trovimap.com>
2019-12-17 00:21:56 +0100

    update

--- hpa-esempio/00-check.sh
+++ hpa-esempio/00-check.sh
@@ -1,2 +1,3 @@
-microk8s.status --wait-ready | grep -i enabled > /tmp/enabled
-grep dns /tmp/enabled && grep metrics-server /tmp/enabled
+#!/bin/bash
+microk8s.status --wait-ready | grep -i enabled > /tmp/enabled.txt
+grep dns /tmp/enabled.txt && grep metrics-server /tmp/enabled.txt
